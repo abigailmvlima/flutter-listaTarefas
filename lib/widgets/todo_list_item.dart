c7caba383957372caf090d91b7f1be938a52cbbf
abigail.lima <abigailmvlima@outlook.com>
2024-07-19 13:00:41 -0300

    criei botÃão delete

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import '../models/todo.dart';
 
@@ -9,31 +10,52 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.grey[300],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Slidable(
+          // Specify a key if the Slidable is dismissible.
+          key: const ValueKey(0),
+      // The end action pane is the one at the right or the bottom side.
+      endActionPane: const ActionPane(
+      motion: ScrollMotion(),
+      children: [
+      SlidableAction(
+      // An action can be bigger than the others.
+        onPressed: null,
+        backgroundColor: Color(0xFFFE4A49),
+        foregroundColor: Colors.white,
+        icon: Icons.delete,
+        label: 'Delete',
       ),
-      margin: const EdgeInsets.symmetric(vertical: 4),
+      ],
+      ),
+      // The child of the Slidable is what the user sees when the
+      child: Container(
+      decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(5),
+      color: Colors.grey[300],
+      ),
       padding: const EdgeInsets.all(16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat('dd/MM/yyyy' ).format(todo.dateTime),
-            style: const TextStyle(
-              fontSize: (12),
-            ),
-          ),
-          Text(
-            todo.title,
-            style: const TextStyle(
-              fontSize: (16),
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ],
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+      Text(
+      DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+      style: const TextStyle(
+      fontSize: (12),
       ),
+      ),
+      Text(
+      todo.title,
+      style: const TextStyle(
+      fontSize: (16),
+      fontWeight: FontWeight.w600,
+      ),
+      ),
+      ],
+      ),
+      ), ),
     );
   }
 }
+
